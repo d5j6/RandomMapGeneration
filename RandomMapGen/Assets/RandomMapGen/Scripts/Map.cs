9413429d93e0e361d9b9f9303c170069abb02b45
Jeffrey von Rotz <jeffreyrotz@gmail.com>
2016-06-09 19:17:19 +0200

    Neighbours are shown and spriteID is known

---
+++ RandomMapGen/Assets/RandomMapGen/Scripts/Map.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map {
+
+    public Tile[] tiles;
+    public int columns;
+    public int rows;
+
+    public void NewMap(int width, int height)
+    {
+        columns = width;
+        rows = height;
+
+        tiles = new Tile[columns * rows];
+
+        CreateTiles();
+    }
+
+    private void CreateTiles()
+    {
+        var total = tiles.Length;
+
+        for (int i = 0; i < total; i++)
+        {
+            var tile = new Tile();
+            tile.id = i;
+
+            tiles[i] = tile;
+        }
+
+        FindNeighbours();
+    }
+
+
+    private void FindNeighbours()
+    {
+        for (var r = 0; r < rows; r++)
+        {
+            for (var c = 0;c  < columns; c++)
+            {
+                var tile = tiles[columns * r + c];
+
+                if (r < rows - 1)
+                {
+                    tile.AddNeighbour(Sides.Bottom, tiles[columns * (r + 1) + c]);
+                }
+
+                if (c < columns - 1)
+                {
+                    tile.AddNeighbour(Sides.Right, tiles[columns * r + c + 1]);
+                }
+
+                if (c>0)
+                {
+                    tile.AddNeighbour(Sides.Left, tiles[columns * r + c - 1]);
+                }
+
+                if (r > 0)
+                {
+                    tile.AddNeighbour(Sides.Top, tiles[columns * (r - 1) + c]);
+                }
+
+            }
+        }
+
+
+    }
+}
---
+++ RandomMapGen/Assets/RandomMapGen/Scripts/Tile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Text;
+
+public enum Sides
+{
+    Bottom,
+    Right,
+    Left,
+    Top
+}
+
+public class Tile {
+
+    public int id = 0;
+    public Tile[] neighbours = new Tile[4];
+    public int autotileID;
+
+    public void AddNeighbour(Sides side, Tile tile)
+    {
+        neighbours[(int)side] = tile;
+        CalculateAutotileID();
+    }
+
+    private void CalculateAutotileID()
+    {
+        var sideValues = new StringBuilder();
+        foreach(Tile tile in neighbours)
+        {
+            sideValues.Append(tile == null ? "0" : "1");
+        }
+
+        autotileID = Convert.ToInt32(sideValues.ToString(), 2);
+
+    }
+}
